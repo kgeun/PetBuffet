5d2533c9e3ae87eb8de532d0b9c89f1ce06381d9
KyungGeun Lee <kyunggeun.lee@navercorp.com>
2018-06-23 21:31:16 +0900

    Doing adding knob plugin into list page

--- configs/mysql.js
+++ configs/mysql.js
@@ -1,12 +1,23 @@
 const mysql = require('promise-mysql');
 let pool;
 
+/*
 pool = mysql.createPool({
     host: '10.105.185.181',
     user: 'root',
     database: 'petbuffet',
     password: 'dlrudrms',
     port: '13306',
+    connectionLimit: 5
+});
+*/
+
+pool = mysql.createPool({
+    host: 'sql12.freemysqlhosting.net',
+    user: 'sql12241516',
+    database: 'sql12241516',
+    password: 'bwHklI8lF3',
+    port: '3306',
     connectionLimit: 5
 });
 
